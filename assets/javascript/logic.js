222b763255bf40b069dc26340df60803f51c09f5
brijamfitz <brijamfitz@Brians-MacBook-Pro.local>
2018-09-12 23:30:14 -0400

    work in progress

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,20 +12,56 @@ firebase.initializeApp(config);
 // Create a variable to reference the database.
 var database = firebase.database();
 
-$('.submit-button').on('click', function(event){
+// On click event to retrieve user input and push to database
+$('#submit-button').on('click', function(event){
+
     event.preventDefault();
-    var trainName = $('#train-name').val().trim();
-    console.log(trainName);
+
+    var name = $('#name').val().trim();
+    console.log(name);
     var destination = $('#destination').val().trim();
     console.log(destination);
-    var trainTime = $('#train-time').val().trim();
-    console.log(trainTime);
+    var firstTrain = $('#first-train').val().trim();
+    console.log(firstTrain);
     var frequency = $('#frequency').val().trim();
     console.log(frequency);
 
-    $('#name').append('<div>' + trainName + '</div>');
-    $('#dest').append('<div>' + destination + '</div>');
-    $('#freq').append('<div>' + frequency + '</div>');
-    $('#arrival').append('<div>' + trainTime + '</div>');
+    // Variable stores all our data
+    var trainInfo = {
+      name: name,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: frequency
+    }
 
-}) 
+    // Code for handling push to firebase database
+    database.ref().push(trainInfo);
+
+    // Clear out input boxes
+    $('#name').val('');
+    $('#destination').val('');
+    $('#first-train').val('');
+    $('#frequency').val('');
+
+})
+
+// Firebase watcher
+database.ref().on('child_added', function(childsnapshot) {
+
+  console.log(childsnapshot.val());
+
+  // Log the values from the database
+  var name = (childsnapshot.val().name);
+  var destination = (childsnapshot.val().destination);
+  var frequency = (childsnapshot.val().frequency);
+
+  // Update the HTML
+  $('#train-name').append('<p>' + name + '</p>');
+  $('#train-destination').append('<p>' + destination + '</p>');
+  $('#train-frequency').append('<p>' + frequency + '</p>');
+
+},
+// Create Error Handling
+function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
